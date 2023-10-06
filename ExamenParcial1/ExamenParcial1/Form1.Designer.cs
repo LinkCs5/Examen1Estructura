c850902d35ad3b446a89cfe5b2abd116c0df7cb5
LinkCs5 <90697790+LinkCs5@users.noreply.github.com>
2023-10-05 18:47:38 -0600

    Add files via upload

---
+++ ExamenParcial1/ExamenParcial1/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace ExamenParcial1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtGenero = new System.Windows.Forms.TextBox();
+            this.txtDiagnostico = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dgvPacientes = new System.Windows.Forms.DataGridView();
+            this.lblTotalPacientes = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblEdadPromedio = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPacientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.Color.SlateBlue;
+            this.txtNombre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNombre.Location = new System.Drawing.Point(12, 38);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.BackColor = System.Drawing.Color.SlateBlue;
+            this.txtEdad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEdad.Location = new System.Drawing.Point(118, 38);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 1;
+            // 
+            // txtGenero
+            // 
+            this.txtGenero.BackColor = System.Drawing.Color.SlateBlue;
+            this.txtGenero.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtGenero.Location = new System.Drawing.Point(224, 38);
+            this.txtGenero.Name = "txtGenero";
+            this.txtGenero.Size = new System.Drawing.Size(100, 20);
+            this.txtGenero.TabIndex = 2;
+            // 
+            // txtDiagnostico
+            // 
+            this.txtDiagnostico.BackColor = System.Drawing.Color.SlateBlue;
+            this.txtDiagnostico.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDiagnostico.Location = new System.Drawing.Point(436, 38);
+            this.txtDiagnostico.Name = "txtDiagnostico";
+            this.txtDiagnostico.Size = new System.Drawing.Size(100, 20);
+            this.txtDiagnostico.TabIndex = 3;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.BackColor = System.Drawing.Color.SlateBlue;
+            this.txtTelefono.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTelefono.Location = new System.Drawing.Point(330, 38);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefono.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(115, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Edad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(221, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Genero";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(433, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Diagnostico";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(327, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Telefono";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnAgregar.Location = new System.Drawing.Point(553, 35);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 10;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnBuscar.Location = new System.Drawing.Point(646, 35);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 11;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnEliminar.Location = new System.Drawing.Point(739, 35);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 12;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // dgvPacientes
+            // 
+            this.dgvPacientes.BackgroundColor = System.Drawing.Color.MediumPurple;
+            this.dgvPacientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPacientes.Location = new System.Drawing.Point(12, 84);
+            this.dgvPacientes.Name = "dgvPacientes";
+            this.dgvPacientes.Size = new System.Drawing.Size(733, 150);
+            this.dgvPacientes.TabIndex = 13;
+            // 
+            // lblTotalPacientes
+            // 
+            this.lblTotalPacientes.AutoSize = true;
+            this.lblTotalPacientes.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblTotalPacientes.Location = new System.Drawing.Point(790, 184);
+            this.lblTotalPacientes.Name = "lblTotalPacientes";
+            this.lblTotalPacientes.Size = new System.Drawing.Size(13, 13);
+            this.lblTotalPacientes.TabIndex = 14;
+            this.lblTotalPacientes.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(757, 147);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Total de Pacientes: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label7.Location = new System.Drawing.Point(757, 95);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Edad Promedio: ";
+            // 
+            // lblEdadPromedio
+            // 
+            this.lblEdadPromedio.AutoSize = true;
+            this.lblEdadPromedio.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblEdadPromedio.Location = new System.Drawing.Point(790, 124);
+            this.lblEdadPromedio.Name = "lblEdadPromedio";
+            this.lblEdadPromedio.Size = new System.Drawing.Size(13, 13);
+            this.lblEdadPromedio.TabIndex = 17;
+            this.lblEdadPromedio.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(871, 337);
+            this.Controls.Add(this.lblEdadPromedio);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblTotalPacientes);
+            this.Controls.Add(this.dgvPacientes);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtDiagnostico);
+            this.Controls.Add(this.txtGenero);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Hospital El Faro del Mayab";
+            this.TransparencyKey = System.Drawing.SystemColors.ButtonShadow;
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPacientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtGenero;
+        private System.Windows.Forms.TextBox txtDiagnostico;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView dgvPacientes;
+        private System.Windows.Forms.Label lblTotalPacientes;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblEdadPromedio;
+    }
+}
+
---
+++ ExamenParcial1/ExamenParcial1/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace ExamenParcial1
+{
+    public partial class Form1 : Form
+    {
+        private RegistroPacientes registroPacientes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            registroPacientes = new RegistroPacientes();
+            dgvPacientes.AutoGenerateColumns = true; // Configurar las columnas automáticamente
+            dgvPacientes.ReadOnly = true;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            // Obtén los datos de los TextBox y crea un nuevo paciente
+                Paciente nuevoPaciente = new Paciente
+                {
+                    Nombre = txtNombre.Text,
+                    Edad = int.Parse(txtEdad.Text),
+                    Genero = txtGenero.Text,
+                    NumeroTelefono = txtTelefono.Text,
+                    DiagnosticoMedico = txtDiagnostico.Text,
+
+                };
+
+                // Agrega el paciente al registro
+                registroPacientes.AgregarPaciente(nuevoPaciente);
+
+                // Actualiza el DataGridView
+                ActualizarDataGridView();
+
+                // Limpia los TextBox
+                LimpiarTextBox();
+                MostrarEstadisticas();
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string nombreABuscar = txtNombre.Text;
+            List<Paciente> pacientesEncontrados = registroPacientes.BuscarPacientePorNombre(nombreABuscar);
+
+            // Actualiza el DataGridView con los pacientes encontrados
+            dgvPacientes.DataSource = pacientesEncontrados;
+
+            // Actualiza las estadísticas
+            MostrarEstadisticas();
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            // Obtén el paciente seleccionado en el DataGridView
+            if (dgvPacientes.SelectedRows.Count > 0)
+            {
+                Paciente pacienteSeleccionado = (Paciente)dgvPacientes.SelectedRows[0].DataBoundItem;
+
+                // Elimina el paciente del registro
+                registroPacientes.EliminarPaciente(pacienteSeleccionado);
+
+                // Actualiza el DataGridView
+                ActualizarDataGridView();
+
+                // Actualiza las estadísticas
+                MostrarEstadisticas();
+            }
+        }
+
+        private void ActualizarDataGridView()
+        {
+            dgvPacientes.DataSource = null; // Limpia la fuente de datos
+            dgvPacientes.DataSource = registroPacientes.ObtenerListaPacientes();
+        }
+
+        private void MostrarEstadisticas()
+        {
+            int totalPacientes = registroPacientes.ObtenerTotalPacientes();
+            double edadPromedio = registroPacientes.ObtenerEdadPromedio();
+
+            lblTotalPacientes.Text = $"Total de Pacientes: {totalPacientes}";
+            lblEdadPromedio.Text = $"Edad Promedio: {edadPromedio:F2}";
+        }
+
+        private void LimpiarTextBox()
+        {
+            txtNombre.Clear();
+            txtEdad.Clear();
+            txtGenero.Clear();
+            txtTelefono.Clear();
+            txtDiagnostico.Clear();
+        }
+    }
+}
+public class Paciente
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public string Genero { get; set; }
+    public string NumeroTelefono { get; set; }
+    public string DiagnosticoMedico { get; set; }
+}
+
+public class RegistroPacientes
+{
+    private List<Paciente> pacientes;
+
+    public RegistroPacientes()
+    {
+        pacientes = new List<Paciente>();
+    }
+
+    public void AgregarPaciente(Paciente paciente)
+    {
+        pacientes.Add(paciente);
+    }
+
+    public List<Paciente> BuscarPacientePorNombre(string nombre)
+    {
+        return pacientes.Where(p => p.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase)).ToList();
+    }
+    public void EliminarPaciente(Paciente paciente)
+    {
+        pacientes.Remove(paciente);
+    }
+
+    public List<Paciente> ObtenerListaPacientes()
+    {
+        return pacientes;
+    }
+
+    public int ObtenerTotalPacientes()
+    {
+        return pacientes.Count;
+    }
+
+    public double ObtenerEdadPromedio()
+    {
+        if (pacientes.Count == 0)
+        {
+            return 0;
+        }
+        double sumaEdades = pacientes.Sum(p => p.Edad);
+        return sumaEdades / pacientes.Count;
+    }
+}
